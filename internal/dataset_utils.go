987d444cf0e8efb3ac860859c6e6b36397c6f4ea
CynthiaOketch <cynthiaoketch6@gmail.com>
2025-03-13 21:58:08 +0300

    feat: added function to get all the unique values of a categorical column

--- internal/dataset_utils.go
+++ internal/dataset_utils.go
@@ -2,6 +2,8 @@ package internal
 
 import "fmt"
 
+const TypeCategorical ColumnType = iota // 0
+
 // GetColIndx returns the index of a column with the given name
 func (d *Dataset) GetColIndex(name string) (int, error) {
 	index, exists := d.ColumnMap[name]
@@ -17,5 +19,31 @@ func (d *Dataset) GetColValues(name string) ([]string, []bool, error) {
 	if err != nil {
 		return nil, nil, err
 	}
-	return d.Columns[index].Values, d.Columns[idx].Missing, nil
+	return d.Columns[index].Values, d.Columns[index].Missing, nil
+}
+
+// GetUniqueValues returns the unique values in a categorical column
+func (d *Dataset) GetUniqueValues(columnName string) ([]string, error) {
+	idx, err := d.GetColIndex(columnName)
+	if err != nil {
+		return nil, err
+	}
+
+	if d.Columns[idx].Type != TypeCategorical {
+		return nil, fmt.Errorf("column '%s' is not categorical", columnName)
+	}
+
+	uniqueMap := make(map[string]bool)
+	for i, val := range d.Columns[idx].Values {
+		if !d.Columns[idx].Missing[i] {
+			uniqueMap[val] = true
+		}
+	}
+
+	uniqueValues := make([]string, 0, len(uniqueMap))
+	for val := range uniqueMap {
+		uniqueValues = append(uniqueValues, val)
+	}
+
+	return uniqueValues, nil
 }
