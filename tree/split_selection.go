5bd4826c66618e76c8568bb773ce12569fb481ad
CynthiaOketch <cynthiaoketch6@gmail.com>
2025-03-14 04:22:31 +0300

    feat: Add filtering of available attributes for split selection

--- tree/split_selection.go
+++ tree/split_selection.go
@@ -17,5 +17,34 @@ func (b *Builder) findBestSplit(data *internal.Dataset, usedAttributes []string)
 		return "", nil, 0, nil
 	}
 
+	// Filter out attributes that have been used
+	availableAttrs := make([]string, 0)
+	for _, col := range data.Columns {
+		// Skip the target column
+		if col.Name == b.targetColumn {
+			continue
+		}
+
+		// Skip used attributes for categorical features
+		if col.Type == internal.CategoricalType {
+			isUsed := false
+			for _, usedAttr := range usedAttributes {
+				if col.Name == usedAttr {
+					isUsed = true
+					break
+				}
+			}
+			if isUsed {
+				continue
+			}
+		}
+
+		availableAttrs = append(availableAttrs, col.Name)
+	}
+
+	// If no attributes available, return empty result
+	if len(availableAttrs) == 0 {
+		return "", nil, 0, nil
+	}
 	return "", nil, 0, nil
 }
